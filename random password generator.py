e0cfa42b67a2cea050193b2f06c94126eb8ab5cd
ziad emad <70884154+Blurdroid@users.noreply.github.com>
2023-01-02 20:25:25 +0200

    Update random password generator.py

--- random password generator.py
+++ random password generator.py
@@ -1,4 +1,4 @@
-#import string module
+#import string, random module
 import string 
 import random
 
@@ -11,45 +11,41 @@ s4 = list(string.punctuation) #;'.
 #take nuumber from user
 character_number = int(input("please enter the number: "))
 
-#make sure that the number is 6 or more
-# while True:
-#     try: 
-#         character_number = int(character_number)
-#         if character_number < 6:
-#             print("you need at least 6 characters")
-#             character_number = input("how many character for the password: ")
-#         else:
-#             break    
-#     except:
-          if character_number != 
-#         print("please enter numbers only")        
-#         character_number = input("please enter the number: ")
+#make sure that the number is 6 or more using try and except 
+while True:
+    try: 
+        character_number = int(character_number)
+        if character_number < 6:
+            print("you need at least 6 characters")
+            character_number = input("how many character for the password: ")
+        else:
+            break    
+    except:
+          print("only numbers please")
+          character_number = input("please enter the number: ")
+   
 
-if character_number < 6:
-    character_number = input("please enter numbers onlyy")    
-
-# #         
+#shuffling all lowercase and upper case      
 random.shuffle(s1)
 random.shuffle(s2)
 random.shuffle(s3)
 random.shuffle(s4)     
         
-#
+#split 30% and 20% from the user's input
 part_1 = round(character_number *(30/100))  #5.4 --> 5  5.6 --> 6
 part_2 = round(character_number *(20/100))
 
-print(part_1, part_2)
+password = []
 
-# password = []
-
-# for i in range(part_1):
-#     password.append(s1[i]) 
-#     password.append(s2[i]) 
+#make a 60% letter and the rest is digits 
+for i in range(part_1):
+    password.append(s1[i]) 
+    password.append(s2[i]) 
        
-# for i in range(part_2):
-#     password.append(s3[i]) 
-#     password.append(s4[i])    
+for i in range(part_2):
+    password.append(s3[i]) 
+    password.append(s4[i])    
 
-# password = "".join(password[0:])
+password = "".join(password[0:])
 
-# print(password)
+print(password)
